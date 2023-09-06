19fb0889f8c162f15a760ad8b282319d4670e08d
vineet <rvineet02@gmail.com>
2023-09-05 23:14:18 -0400

    temp

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,157 @@
+-- Install package manager
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- NOTE: Here is where you install your plugins.
+--  You can configure plugins using the `config` key.
+--
+--  You can also configure plugins after the setup call,
+--    as they will be available in your neovim runtime.
+require('lazy').setup({
+  -- NOTE: First, some plugins that don't require any configuration
+
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+  'christoomey/vim-tmux-navigator',
+
+  -- Useful plugin to show you pending keybinds.
+  { 'folke/which-key.nvim',  opts = {} },
+  {
+    -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+  },
+
+  {
+    -- Theme inspired by Atom
+    'chriskempson/base16-vim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'base16-gruvbox-dark-hard'
+    end,
+  },
+
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'base16',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  -- Fuzzy Finder (files, lsp, etc)
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    config = function()
+      require("plugins.telescope")
+    end,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+  },
+
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+    config = function()
+      require("plugins.treesitter")
+    end,
+  },
+
+  {
+    -- Copilot
+    "zbirenbaum/copilot.lua",
+    config = function()
+      require("plugins.copilot")
+    end,
+    event = "InsertEnter",
+  },
+
+  {
+    -- Nvim-tree
+    'nvim-tree/nvim-tree.lua',
+    version = "*",
+    lazy = false,
+    config = function() require('plugins.nvim-tree') end,
+  },
+
+  {
+    -- Nvim dev icons
+    'nvim-tree/nvim-web-devicons',
+  },
+
+  -- LSP + Autocomplete
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    config = function() require('plugins.lsp') end,
+    dependencies = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },             -- Required
+      { 'williamboman/mason.nvim' },           -- Optional
+      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },         -- Required
+      { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+      { 'hrsh7th/cmp-buffer' },       -- Optional
+      { 'hrsh7th/cmp-path' },         -- Optional
+      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+      { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },             -- Required
+      { 'rafamadriz/friendly-snippets' }, -- Optional
+
+      { 'simrat39/rust-tools.nvim' }
+    }
+  },
+
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+}, {})
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,77 @@
+local lsp = require('lsp-zero').preset('recommended')
+
+lsp.ensure_installed({
+  'tsserver',
+  'lua_ls',
+  'gopls',
+  'jdtls',
+  'rust_analyzer'
+})
+
+-- Preferences
+lsp.set_preferences({
+  suggest_lsp_servers = false
+})
+
+
+-- Autocompletion
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+vim.g.copilot_no_tab_map = true
+lsp.setup_nvim_cmp({
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = "luasnip" },
+    { name = "path" },
+    { name = 'buffer' }
+  },
+  mapping = {
+    ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+        local copilot = require("copilot.suggestion")
+        if cmp.visible() then
+          cmp.confirm({ select = false })
+        elseif copilot.is_visible() then
+          copilot.accept()
+        else
+          fallback()
+        end
+      end,
+      {
+        "i",
+        "s"
+      }),
+  }
+})
+
+-- Keybindings
+lsp.on_attach(function(client, bufnr)
+  local opts = { buffer = bufnr, remap = false }
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+  vim.keymap.set("n", "<C-k>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set('n', '<LEADER>D', function() vim.lsp.buf.type_definition() end, opts)
+  vim.keymap.set('n', '<LEADER>r', function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set('n', '<LEADER>a', function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set('n', '<LEADER>f', function() vim.lsp.buf.format({ async = true }) end, opts)
+  vim.keymap.set('n', '<LEADER>w', function()
+    vim.lsp.buf.format()
+    vim.cmd('w')
+  end, opts)
+  vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
+end)
+
+lsp.setup()
+
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  float = true,
+})
